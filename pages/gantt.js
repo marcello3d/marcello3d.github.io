222bd43e1bf4a6385c0c9417c26859f582892843
Marcello Bastea-Forte <marcello@cellosoft.com>
2018-05-15 22:18:15 -0700

    it works!

---
+++ pages/gantt.js
@@ -0,0 +1,290 @@
+import data from '../data/data.yaml'
+import FAIcon from '@fortawesome/react-fontawesome'
+
+import faPalette from '@fortawesome/fontawesome-pro-solid/faPalette'
+import faFilm from '@fortawesome/fontawesome-pro-solid/faFilm'
+import faBrowser from '@fortawesome/fontawesome-pro-regular/faBrowser'
+import faRuler from '@fortawesome/fontawesome-pro-solid/faRuler'
+import faGamepad from '@fortawesome/fontawesome-pro-solid/faGamepad'
+import faToolbox from '@fortawesome/fontawesome-pro-solid/faToolbox'
+import faMobileAlt from '@fortawesome/fontawesome-pro-solid/faMobileAlt'
+import faDesktop from '@fortawesome/fontawesome-pro-solid/faDesktop'
+import faMousePointer from '@fortawesome/fontawesome-pro-solid/faMousePointer'
+
+const MONTHS = {
+  1: 'Jan',
+  2: 'Feb',
+  3: 'Mar',
+  4: 'Apr',
+  5: 'May',
+  6: 'Jun',
+  7: 'Jul',
+  8: 'Aug',
+  9: 'Sep',
+  10: 'Oct',
+  11: 'Nov',
+  12: 'Dec'
+}
+
+function parseDate(dateString) {
+  const [year, month, day] = dateString.split('.').map((s) => parseInt(s, 10))
+  return { year, month, day }
+}
+
+let minYear = Infinity
+let maxYear = -Infinity
+
+function getPlace(place, orDefault = { name: place }) {
+  return data.places[place] || orDefault
+}
+
+function getTechnology(id, orDefault = { name: id }) {
+  return data.technologies[id] || orDefault
+}
+
+for (const project of Object.values(data.projects)) {
+  const { date, place, tech } = project
+  const [startDate, endDate] = date.split(/\s*-\s*|\sto\s|,/)
+  const start = parseDate(startDate)
+  const end = parseDate(endDate)
+  if (start.year < minYear) {
+    minYear = start.year
+  }
+  if (end.year > maxYear) {
+    maxYear = end.year
+  }
+  project.startDate = start
+  project.endDate = end
+  if (place && !getPlace(place, false)) {
+    console.error(`${place} not specified in data.places`)
+  }
+  if (!tech) {
+    project.tech = []
+  } else {
+    for (const id of tech) {
+      if (!getTechnology(id, false)) {
+        console.error(`${id} not specified in data.tech`)
+      }
+    }
+  }
+}
+
+const yearMonths = []
+for (let year = maxYear; year >= minYear; year--) {
+  for (let month = 12; month >= 1; month--) {
+    yearMonths.push({
+      year,
+      month,
+      projects: Object.values(data.projects).filter(
+        ({ endDate }) => endDate.year === year && endDate.month === month
+      )
+    })
+  }
+}
+
+function getIcon(type) {
+  switch (type) {
+    case 'art':
+      return faPalette
+    case 'film':
+      return faFilm
+    case 'app':
+      return faBrowser
+    case 'mobile':
+      return faMobileAlt
+    case 'design':
+      return faRuler
+    case 'game':
+      return faGamepad
+    case 'library':
+      return faToolbox
+    case 'web':
+      return faDesktop
+    default:
+      return faMousePointer
+  }
+}
+
+function formatDate({ year, month }) {
+  if (month) {
+    return `${MONTHS[month]} ${year}`
+  }
+  return `${year}`
+}
+function formatDates(start, end) {
+  if (start.year === end.year && start.month === end.month) {
+    return formatDate(start)
+  }
+  return `${formatDate(start)} - ${formatDate(end)}`
+}
+
+function Project({
+  name,
+  launched,
+  launchedDate,
+  status,
+  type,
+  date,
+  startDate,
+  endDate,
+  place,
+  tech
+}) {
+  return (
+    <div className={`project ${type}`}>
+      <style jsx>{/*language=CSS*/ `
+        .project {
+          display: flex;
+          flex-direction: column;
+          justify-content: stretch;
+          flex-grow: 1;
+          height: 100%;
+          padding: 8px 10px;
+          width: 10em;
+          background: #adb8d4;
+          border-radius: 4px;
+          box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .content {
+          flex-grow: 1;
+        }
+
+        .date,
+        .details {
+          font-size: 80%;
+        }
+
+        .type {
+          background: #c9c9c9;
+          color: #fff;
+          display: inline-block;
+          font-weight: bold;
+          padding: 2px 4px 3px;
+          border-radius: 6px;
+          font-size: 80%;
+        }
+
+        .art {
+          background: #00cac2;
+        }
+
+        .film {
+          background: #555;
+          color: #fff;
+        }
+
+        .design {
+          background: #ca1b5a;
+        }
+
+        .web {
+          background: #f39a00;
+        }
+
+        .game {
+          background: #b14bff;
+        }
+
+        .library {
+          background: #0da858;
+        }
+
+        .app {
+          background: #4f88ff;
+        }
+
+        .mobile {
+          background: #4056ff;
+          color: #fff;
+        }
+      `}</style>
+      <div className="date">
+        <FAIcon icon={getIcon(type)} /> {formatDates(startDate, endDate)}
+      </div>
+      <div className="header">
+        <strong className="name">{name}</strong>
+        {place && ` @ ${getPlace(place).name}`}
+      </div>
+      {status !== 'released' && (
+        <div className="date">
+          <em>{status}</em>
+        </div>
+      )}
+      <div className="details">
+        {tech.map((id) => getTechnology(id).name).join(', ')}
+      </div>
+    </div>
+  )
+}
+
+export default () => (
+  <div id="content">
+    <style global jsx>{/*language=CSS*/ `
+      html {
+        font-family: 'Pontano Sans', 'Helvetica Neue', Helvetica, Arial,
+          sans-serif;
+        color: #000;
+        background: #fff;
+        font-size: 12pt;
+      }
+
+      body {
+        max-width: 45em;
+        width: 100%;
+        margin: 0 auto;
+      }
+      .even-year {
+        background: #eee;
+      }
+      .year {
+        vertical-align: top;
+        font-weight: bold;
+      }
+      .month {
+        height: 5px;
+      }
+      table {
+        border-spacing: 1px;
+        border-collapse: collapse;
+      }
+      table td {
+        vertical-align: top;
+        margin: 0;
+      }
+      tr,
+      td {
+        height: 1px;
+      }
+    `}</style>
+    <table>
+      <tbody>
+        {yearMonths.map(({ year, month, projects }) => {
+          return (
+            <tr key={year + '.' + month}>
+              {month === 12 ? (
+                <td className="year" rowSpan="12">
+                  {year}
+                </td>
+              ) : null}
+              <td className="month" />
+              {projects.map((project, index) => (
+                <td
+                  key={index}
+                  rowSpan={
+                    project.endDate.year * 12 +
+                    project.endDate.month -
+                    (project.startDate.year * 12 + project.startDate.month) +
+                    1
+                  }
+                >
+                  <Project {...project} />
+                </td>
+              ))}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,156 @@
+export default () => (
+  <div id="content">
+    <style global jsx>{ /*language=CSS*/ `
+      html {
+    font-family: 'Pontano Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: #000;
+    background: #fff;
+    font-size: 12pt;
+}
+body {
+    max-width: 45em;
+    width: 100%;
+    margin: 0 auto;
+}
+
+h1,h2 {
+    margin: 0;
+    padding: 0
+}
+
+a {
+    color: #FA0F14;
+}
+
+h1 {
+    font-size: 28.9pt;
+    margin-top: -0.2em;
+    margin-bottom: 0.5em;
+}
+
+h2 {
+    font-size: 15pt;
+    text-align: center;
+}
+
+p {
+    margin: 0.6em 0;
+}
+
+h1,h2 {
+    font-weight:normal;
+    letter-spacing: 0.1em;
+}
+
+#content {
+    position: relative;
+    padding: 8em;
+}
+
+#box1,#box2,#box3 {
+    z-index: -1;
+    font-size: 12pt;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    border: solid 1em;
+    border-radius: 2em;
+}
+
+#box1 {
+    border-color: rgba(238, 198, 135, 0.40);
+    border-top-left-radius: 6em;
+    margin: 1em 5em 5em 1em;
+}
+
+#box2 {
+    border-color: rgba(54, 53, 255, 0.4);
+    border-top-left-radius: 4em;
+    border-bottom-right-radius: 4em;
+    margin: 3em;
+}
+
+#box3 {
+    border-color: rgba(250, 15, 20, 0.4);
+    border-bottom-right-radius: 6em;
+    margin: 5em 1em 1em 5em;
+}
+
+span.time {
+    color: #485277;
+    font-size: 80%;
+    letter-spacing: 0.05em;
+    white-space: nowrap;
+}
+
+ul {
+    margin: 1em 0;
+    padding: 0;
+}
+
+li {
+    list-style: none;
+    padding-bottom: 0.5em;
+}
+    `}</style>
+    <div id="box1"></div>
+    <div id="box2"></div>
+    <div id="box3"></div>
+    <h1>Marcello Bastéa-Forte</h1>
+    <h2>About</h2>
+    <p>I code, draw, hack, and design. It's hard to say where one starts and the other ends.</p>
+    <p>For the freshest, do check my <a href="https://github.com/marcello3d">github</a> and
+      <a href="http://twitter.com/marcello3d">twitter</a>.</p>
+    <p>You can email me at marcello<code>@</code>cellosoft<code>.</code>com.</p>
+    <p>Thanks for visiting!</p>
+    <h2>Current projects</h2>
+    <ul>
+      <li><a href="http://viv.ai/">viv.ai</a> — nlp/ai platform <span className="time">(since 2013)</span></li>
+      <li><a href="http://2draw.net/">2draw.net</a> &mdash;
+        online drawing tools + community
+        <small>(<a href="http://2draw.net/users/marcello/gallery/">my drawings</a>)</small>
+        <span className="time">(since 2002)</span></li>
+
+      <li><a href="https://npmjs.org/~marcello">various node.js modules</a> &mdash;
+        available via npm and github <span className="time">(since 2011)</span></li>
+    </ul>
+    <h2>Past projects</h2>
+    <ul>
+      <li><a href="https://github.com/marcello3d/node-mongolian">Mongolian DeadBeef</a> &mdash;
+        mongodb driver for node.js <span className="time">(2011 to 2012)</span></li>
+      <li><a href="http://www.siri.com">Siri</a> — virtual personal assistant <span className="time">(2008 to 2012)</span>
+      </li>
+      <li><a href="http://jtablet.cellosoft.com/">JTablet Plugin</a> &mdash;
+        Java library for accessing graphics tablet input <span className="time">(2003 to 2012)</span></li>
+      <li><a href="http://alt.cellosoft.com/">Alt Framework</a> &mdash;
+        server-side javascript web framework <span className="time">(2006 to 2009)</span></li>
+    </ul>
+    <h2>Archived projects</h2>
+    <p>(courtesy of <a href="https://web.archive.org/">archive.org</a>)</p>
+    <ul>
+      <li><a href="https://web.archive.org/web/20101029202045/http://www.cs.unm.edu/~cello/processing/"
+             className="unavailable">I LIKE PROCESSING</a> &mdash;
+        interactive art java applets made with processing <span className="time">(2004 to 2006)</span></li>
+      <li><a href="https://web.archive.org/web/20130625054816/http://marcello.cellosoft.com/projects/squeal/"
+             className="unavailable">SQueaL with Delight</a> &mdash;
+        xml-based sql schema system prototype <span className="time">(2006)</span></li>
+      <li><a href="https://web.archive.org/web/20090329155558/http://www.cs.unm.edu/~cello/livingescher/"
+             className="unavailable">living escher</a> &mdash;
+        art exhibit in processing <span className="time">(2004)</span></li>
+      <li><a href="https://web.archive.org/web/20070527173519/http://www.cs.unm.edu/~cello/maya/readme.html"
+             className="unavailable">maya</a> &mdash;
+        collection of computer animations (also <a href="http://www.youtube.com/user/marcello3d/videos">on youtube</a>)
+        <span className="time">(2002 to 2004)</span></li>
+      <li><a href="https://web.archive.org/web/20130509082207/http://www.cs.unm.edu/~cello/jsgen/"
+             className="unavailable">jsgen</a> &mdash;
+        utility for bridging C++ classes to JS in Spidermonkey <span className="time">(2004)</span></li>
+      <li><a href="https://web.archive.org/web/20130625053550/http://marcello.cellosoft.com/art/">art/</a> &mdash;
+        old art archive <span className="time">(2001 to 2003)</span></li>
+    </ul>
+    <h2>Have a great day!</h2>
+  </div>
+
+)
